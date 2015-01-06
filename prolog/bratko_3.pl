83952eb9e0b42f2f7856da3706f5a7d1a0441b28
Henry Miskin <hpm14@ic.ac.uk>
2015-01-06 10:30:44 +0000

    Completed exercises from chapter 3

--- prolog/bratko_3.pl
+++ prolog/bratko_3.pl
@@ -45,6 +45,32 @@ subset([SetHead|SetTail],[SubSetHead|SubSetTail]) :-
 subset([_|SetTail],SubSet) :-
 	subset(SetTail,SubSet).
 
+%dividelist takes a List and divedes it into List1 and List2 of similar length
+
+dividelist([],[],[]).
+
+dividelist([E],[E],[]).
+
+dividelist([H1,H2|T],[H1|T1],[H2|T2]) :-
+	dividelist(T,T1,T2).
+	
+
+%flatten removes all lists within a parent list
+
+flatten([],[]).
+
+flatten([H|T],FL) :-
+	[] = H, !,
+	flatten(T,FL).
+
+flatten([H|T],FL) :-
+	[_|_] = H, !,
+	append(H,T,L),
+	flatten(L,FL).
+
+flatten([H|T],[H|FT]) :-
+	flatten(T,FT).
+
 %max returns te greater of two numbers
 
 max(X,Y,Max) :-
@@ -86,3 +112,17 @@ ordered([Head1|Tail]) :-
 subsum(Set,Sum,SubSet) :-
 	subset(Set,SubSet),
 	sumlist(SubSet,Sum).
+
+%between generates all integers between N1 and N2
+
+makelist(EN,EN,[EN]).
+
+makelist(E1,EN,[E1|T]) :-
+	E2 is E1 + 1,
+	E2 =< EN,
+	makelist(E2,EN,T).
+
+between(N1,N2,X) :-
+	makelist(N1,N2,List),
+	member(X,List).
+
