fb803468793fb94a20c286d067bd670cd64ce4d8
Henry Miskin <henry.miskin@talktalk.net>
2015-01-12 12:01:48 +0000

    Added countries database

---
+++ prolog/countries.pl
@@ -0,0 +1,48 @@
+% Country (Country, Capital, Inhabitants_in_millions,  Area_in_square _kilometers) 
+country ('Sweden', 'Stockholm', 9.3, 449964). 
+country ('Denmark', 'Copenhagen', 5.5, 43094). 
+country ('Norway', 'Oslo', 4.8, 385199). 
+country ('Finland', 'Helsinki', 5.3, 338145). 
+country ('Iceland', 'Reykjavik', 0.32, 103125). 
+country ('Great Britain', 'London', 60.6, 244820). 
+country ('Turkey', 'Ankara', 71.5, 783562). 
+ 
+% Nordic_country (Country) 
+nordic_country ('Sweden'). 
+nordic_country ('Norway'). 
+nordic_country ('Denmark'). 
+nordic_country ('Finland'). 
+nordic_country ('Iceland'). 
+ 
+% National_day (Country, date (Day, Month)) 
+national_day ('Sweden', date (6, 'June')). 
+national_day ('Norge', date (17, 'May')). 
+national_day ('Finland', date (6, 'December')). 
+national_day ('Turkey', date (29, 'October')). 
+national_day ('Denmark', date (5, 'June')). 
+national_day ('Great Britain', date (11, 'June')). 
+national_day ('Iceland', date (17, 'June')). 
+ 
+% Form_of_government (Country, Form) 
+form_of_government ('Sweden', monarchy). 
+form_of_government ('Norway', monarchy). 
+form_of_government ('Finland', republic). 
+form_of_government ('Denmark', monarchy). 
+form_of_government ('Great Britain', monarchy). 
+form_of_government ('Iceland', republic). 
+form_of_government ('Turkey', republic). 
+ 
+% Queen (Country, Name) 
+queen ('Denmark', 'Margrethe II'). 
+queen ('Great Britain', 'Elisabeth II'). 
+ 
+% King (Country, Name) 
+king ('Sweden', 'Karl XVI Gustav'). 
+king ('Norway', 'Harald V'). 
+ 
+% Flag (Country, Colour_list) 
+flag ('Sweden', [blue, yellow]). 
+flag ('Norway', [red, blue, white]). 
+flag ('Finland', [blue, white]). 
+flag ('Denmark', [red, white]). 
+flag ('Great Britain', [blue, white, red]). 
